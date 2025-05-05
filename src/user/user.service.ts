62f027a9a8280f7b0f58215149f689e9504199fe
Antonio Santana <antoniosantana4263@gmail.com>
2025-05-05 14:36:11 -0300

    feat:update user

---
+++ src/user/user.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  private users: User[] = [];
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const saltOrRounds = 10;
+    const passwordHashed = await bcrypt.hash(createUserDto.password, saltOrRounds);
+
+    const user: User = {
+      ...createUserDto,
+      id: this.users.length + 1,
+      password: passwordHashed,
+    };
+
+    this.users.push(user);
+
+    return user
+  }
+
+   async findAll(): Promise<User[]> {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
