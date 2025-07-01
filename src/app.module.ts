e08ceefb4ba4bea5647e9ae7aa3ae6ceef613238
stneds <antoniosantana4263@gmail.com>
2025-07-01 17:18:32 -0300

    fix/conect-bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
-
-
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
-  controllers: [UserController],
-  providers: [UserService],
+  imports: [ConfigModule.forRoot({
+  envFilePath: ['.env.development.local'],
+  }),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    synchronize: true,
+    entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+  })
+
+  , UserModule],
 })
 export class AppModule {}
