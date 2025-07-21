e8ecbb46799b4bbdbf5a67cf228d5399b8ecd466
stneds <antoniosantana4263@gmail.com>
2025-07-21 15:19:43 -0300

    feat/migration-initialization

---
+++ src/migration/1751480390944-create_table_user.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateTableUser1751480390944 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        
+        
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
